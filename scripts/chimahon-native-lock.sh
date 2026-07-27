7b9b37ecce54114094af79f5838b78d456b39268
Autumn Skerritt <skerraut@amazon.com>
2026-07-27 15:14:17 +0000

    fix: consume verified mpv release artifacts

--- scripts/chimahon-native-lock.sh
+++ scripts/chimahon-native-lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-v_aniyomi_mpv_tag=1.18.n-chimahon.2
-v_aniyomi_mpv_commit=6e2b0827e837df6f3414e9530fd547bd867c6820
+v_aniyomi_mpv_tag=1.18.n-chimahon.3
+v_aniyomi_mpv_commit=8f840c1a0b961280ea199104d905bc9567de758f
 v_ffmpeg_reference_commit=ea3d24bbe3c58b171e55fe2151fc7ffaca3ab3d2
 v_cpu_features_commit=936b9ab5515dead115606559502e3864958f7f6e
